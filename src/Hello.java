83ca61ea26441a336f574fa8530ab8ef7dcc8b8c
Mainul <ruman.mainul86@gmail.com>
2018-02-24 20:31:06 +0600

    write new code

--- src/Hello.java
+++ src/Hello.java
@@ -1,8 +1,14 @@
 
 public class Hello {
 
+	
 	public static void main(String[] args) {
+	int a,b, sum;
 		// TODO Auto-generated method stub
+		a =12;
+		b=10;
+		sum = a + b;
+		System.out.println(sum);
 
 	}
 
